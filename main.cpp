b1ba84dda71b4b5231b24d95d3b8286efbecb31a
Edmundo Ruelas Angulo <110996144+SrDesno@users.noreply.github.com>
2024-09-20 18:07:46 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int month;
+
+  //validation
+  do{
+    std::cout<<"Type the month: ";
+    std::cin>>month;
+
+  )  while (month<1 and month >12);
+
+    std::cout<<"ok"<<std::endl;
+}
